d4707fe0319b0c2cca4b8ffcf8b4848fc6698431
Frederick Morrison <frederick.morrison@outlook.com>
2023-10-24 08:11:35 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import sys
+import turtle as t
+
+
+def get_python_version() -> str:
+    return f'{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}'
+
+
+def draw_geometric_pattern():
+    for steps in range(100):
+        for c in ('blue', 'red', 'green'):
+            t.color(c)
+            t.forward(steps)
+            t.right(30)
+    t.mainloop()
+
+
+def draw_star_shape():
+    t.color('red')
+    t.fillcolor('yellow')
+    t.begin_fill()
+    while True:
+        t.forward(200)
+        t.left(170)
+        if abs(t.pos()) < 1:
+            break
+    t.end_fill()
+    t.mainloop()
+
+
+if __name__ == '__main__':
+    print(f'Python version {get_python_version()}')
+    # draw_geometric_pattern()
+    draw_star_shape()
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Python Turtle Graphics Demo
+## References
+[Turtle Graphics Introduction](https://docs.python.org/3/library/turtle.html#introduction)
